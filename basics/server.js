259058ba905049a91808450a92964246d5621d84
Denuwan-Kalubowila <denuwankalubowila@gmail.com>
2024-06-06 19:53:04 +0530

    learn polling and search with htmx

--- basics/server.js
+++ basics/server.js
@@ -31,6 +31,51 @@ app.post("/convert",(req,res)=>{
     `)
 })
 
+// let count=0
+// app.get("/poll",(req,res)=>{
+//     count +=5
+//     const data={
+//         value: count
+//     }
+//     res.json(data)
+// })
+
+let current= 20
+app.get("/get-current-temp",(req,res)=>{
+    current +=Math.random() *2 -1
+    res.send(current.toFixed(1)+"C")
+})
+
+const get_users=async ()=>{
+    const response = await fetch(`https://jsonplaceholder.typicode.com/users`);
+    const data = await response.json();
+    return data
+}
+
+app.post("/search",async(req,res)=>{
+    const uData=await get_users()
+    console.log(uData)
+    const searchName=req.body.search.toLowerCase()
+    if (!searchName){
+        res.send('<tr></tr>')
+    }
+    const searchRes= uData.filter(contact=>{
+        const name= contact.name.toLowerCase();
+        const email= contact.email.toLowerCase();
+
+        return(
+            name.includes(searchName) || email.includes(searchName)
+        )
+    })
+    const searchresHtml=searchRes.map(contact=>`
+    <tr>
+        <td><div class="my-4 p-2">${contact.name}</div></td>
+        <td><div "my-4 p-2">${contact.email}</div></td>
+    </tr>
+    `).join("")
+    res.send(searchresHtml)
+})
+
 app.listen(3000,()=>{
     console.log('Server is runnuing on port 3000');
 })
